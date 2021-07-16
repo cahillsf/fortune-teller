8f07e2723cac4de37eda4ec7aa6cf7671c1d517b
cahillsf <stephen.cahill@datadoghq.com>
2021-07-16 15:56:48 -0400

    update sql script to read from larger csv

--- fortune-teller-mysql/mysql-db-init-files/fortune-teller-mysql-script.sql
+++ fortune-teller-mysql/mysql-db-init-files/fortune-teller-mysql-script.sql
@@ -3,12 +3,14 @@ CREATE DATABASE fortune_teller;
 USE fortune_teller;
 
 CREATE TABLE fortunes(
+	message_id INT PRIMARY KEY,
     quote VARCHAR(500) NOT NULL,
     topic VARCHAR(25) NOT NULL
 );
 
 /* LOAD DATA LOCAL INFILE './app/fortunes.txt'  INTO TABLE fortunes; */
-LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/fortunes.txt'  INTO TABLE fortunes;
+/*LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/fortunes.txt'  INTO TABLE fortunes;*/
+LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/fortunes.csv' INTO TABLE fortunes COLUMNS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
 CREATE USER 'ft_express_db_connect'@'fortune-teller_express_server_1.fortune-teller_default' IDENTIFIED WITH mysql_native_password BY 'tacosandburritos';
 CREATE USER 'datadog'@'datadog-agent-ft.fortune-teller_default' IDENTIFIED WITH mysql_native_password BY 'tacosandburritos';
 GRANT SELECT ON fortune_teller.fortunes to 'ft_express_db_connect'@'fortune-teller_express_server_1.fortune-teller_default';
